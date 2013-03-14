70f770a35522d0e391a036fdce65257a4fee0814
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-14 09:25:57 +0100

    added module credit test

--- spec/app/models/modul_model_spec.rb
+++ spec/app/models/modul_model_spec.rb
@@ -5,6 +5,7 @@ latex_source = <<EOF
 some context
 this shouldn't matter
   \\modulename{name with spaces and ümlauts}
+\\modulecredits{8}
   more context
 EOF
 
@@ -12,5 +13,10 @@ describe "ModulModel" do
   it "gets the module name from latex" do
     modul = Modul.from_latex latex_source
     modul.name.should == "name with spaces and ümlauts"
+  end
+
+  it "gets the credits from latex" do
+    modul = Modul.from_latex latex_source
+    modul.credits.should == 8
   end
 end

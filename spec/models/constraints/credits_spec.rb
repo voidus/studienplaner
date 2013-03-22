a6ec7427f7b951ea243e00831bbcc71cd48ae6c7
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-22 18:33:58 +0100

    test credits constraint

--- spec/models/constraints/credits_spec.rb
+++ spec/models/constraints/credits_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe Constraints::Credits do
-  subject {Constraints::Credits.new 5}
+  subject(:constraint) {Constraints::Credits.new 5}
 
   include ConstraintsInterface
+
+  specify "fails with insufficient credits" do constraint.satisfied_by?(stub(credits: 3)).should be_false end
+  specify "succeeds with sufficient credits" do constraint.satisfied_by?(stub(credits: 9)).should be_true end
 end

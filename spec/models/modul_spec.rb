abc3804205a8253e8f469762bc4447cad9d65ea3
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-22 21:09:54 +0100

    renamed modul_spec

--- spec/models/modul_spec.rb
+++ spec/models/modul_spec.rb
@@ -7,6 +7,7 @@ this shouldn't matter
   \\modulename{name with spaces and ümlauts}
 \\modulecredits{8}
   more context
+  Not a Stammmodul, do not confuse
 EOF
 
 describe Modul do
@@ -16,5 +17,6 @@ describe Modul do
     its(:valid?) {should be_true}
     its(:name) {should eql "name with spaces and ümlauts"}
     its(:credits) {should eql 8}
+    its(:stammmodul?) {should be_false}
   end
 end

6a1828e88e6277970489b1d35816d75186be2558
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-23 03:31:34 +0100

    reindented stammmodul_count constraint

--- models/constraints/stammmodul_count.rb
+++ models/constraints/stammmodul_count.rb
@@ -1,27 +1,27 @@
 module Constraints
-    class StammmodulCount
-        include Jsonizable.new :count
+  class StammmodulCount
+    include Jsonizable.new :count
 
-        attr_accessor :count
+    attr_accessor :count
 
-        def initialize count
-            @count = count
-        end
-
-        def satisfied_by? studienplan
-            studienplan.moduls.to_a.count {|m| m.stammmodul?} >= @count
-        end
-
-        def unfulfilled_message
-            "Der Plan muss mindestens #{@count} Stammodule beinhalten"
-        end
-
-        def fulfilled_message
-            "Mindestens #{@count} Stammmodule"
-        end
-
-        def == o
-            o.instance_of? self.class and o.count == @count
-        end
+    def initialize count
+      @count = count
     end
+
+    def satisfied_by? studienplan
+      studienplan.moduls.to_a.count {|m| m.stammmodul?} >= @count
+    end
+
+    def unfulfilled_message
+      "Der Plan muss mindestens #{@count} Stammodule beinhalten"
+    end
+
+    def fulfilled_message
+      "Mindestens #{@count} Stammmodule"
+    end
+
+    def == o
+      o.instance_of? self.class and o.count == @count
+    end
+  end
 end

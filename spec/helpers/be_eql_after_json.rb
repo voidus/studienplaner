e5e7116a8132ade824ebe639a72f05e122ab04b2
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-23 03:43:14 +0100

    Add a description and helpful message to be_eql_after_json rspec matcher

--- spec/helpers/be_eql_after_json.rb
+++ spec/helpers/be_eql_after_json.rb
@@ -1,5 +1,14 @@
 RSpec::Matchers.define :be_eql_after_json do
   match do |s|
+    @s = s
     JSON.load(JSON.dump(s)) == s
+  end
+
+  failure_message_for_should do |actual|
+    "expected #{@s.inspect} to be eql after json dump-load\n\tinstead got #{actual.inspect}"
+  end
+
+  description do
+    "be the same object after JSON.dump and JSON.load"
   end
 end

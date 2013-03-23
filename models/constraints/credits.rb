7e40e103dcde7fd38d33907d7b705240698b942e
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-23 03:28:29 +0100

    reindented Constraints::Credits

--- models/constraints/credits.rb
+++ models/constraints/credits.rb
@@ -1,36 +1,36 @@
 module Constraints
-    class Credits
-        attr_accessor :credits
+  class Credits
+    attr_accessor :credits
 
-        def initialize credits
-            @credits = credits
-        end
-
-        def satisfied_by? studienplan
-            studienplan.credits >= @credits
-        end
-
-        def unfulfilled_message
-            "Der Plan muss mindestens #{@credits} Leistungspunkte beinhalten"
-        end
-
-        def fulfilled_message
-            "Mindestens #{@credits} Leistungspunkte"
-        end
-
-        def to_json(*a)
-            {
-                'json_class' => self.class.name,
-                'credits' => @credits
-            }.to_json(*a)
-        end
-
-        def self.json_create(o)
-            new o['credits']
-        end
-
-        def == o
-            o.instance_of? self.class and o.credits == @credits
-        end
+    def initialize credits
+      @credits = credits
     end
+
+    def satisfied_by? studienplan
+      studienplan.credits >= @credits
+    end
+
+    def unfulfilled_message
+      "Der Plan muss mindestens #{@credits} Leistungspunkte beinhalten"
+    end
+
+    def fulfilled_message
+      "Mindestens #{@credits} Leistungspunkte"
+    end
+
+    def to_json(*a)
+      {
+        'json_class' => self.class.name,
+        'credits' => @credits
+      }.to_json(*a)
+    end
+
+    def self.json_create(o)
+      new o['credits']
+    end
+
+    def == o
+      o.instance_of? self.class and o.credits == @credits
+    end
+  end
 end

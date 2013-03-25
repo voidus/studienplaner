9c1297b781a41e172bdb46027a3e1c5ed704d6ab
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-25 14:59:14 +0100

    polish be_eql_after_json matcher a little

--- spec/helpers/be_eql_after_json.rb
+++ spec/helpers/be_eql_after_json.rb
@@ -1,11 +1,11 @@
 RSpec::Matchers.define :be_eql_after_json do
   match do |s|
     @s = s
-    JSON.load(JSON.dump(s)) == s
+    JSON.load(JSON.dump(s)).eql? s
   end
 
   failure_message_for_should do |actual|
-    "expected #{@s.inspect} to be eql after json dump-load\n\tinstead got #{actual.inspect}"
+    "expected #{@s.inspect} to be eql after json dump-load\n\tgot     #{actual.inspect} instead"
   end
 
   description do

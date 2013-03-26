4bf6d07840e808c6d2a10026a86e5b36bcfa9c9a
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2013-03-26 13:35:28 +0100

    test for fractional modul credits

--- spec/models/modul_spec.rb
+++ spec/models/modul_spec.rb
@@ -4,4 +4,10 @@ describe Modul do
   it {should validate_presence_of :key}
   it {should validate_presence_of :name}
   it {should validate_presence_of :credits}
+
+  context "fractional credit point" do
+    subject {Modul.new credits: 14.2}
+
+    its(:credits) {should eq 14.2}
+  end
 end

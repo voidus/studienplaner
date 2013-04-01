ab1e827a84b220fae3a97198fffb4defffc32e8a
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-04-01 14:31:00 +0200

    improved be_eql_after_json matcher

--- spec/helpers/be_eql_after_json.rb
+++ spec/helpers/be_eql_after_json.rb
@@ -1,14 +1,27 @@
+def to_json_and_back obj
+  JSON.load(JSON.dump(obj))
+end
+
 RSpec::Matchers.define :be_eql_after_json do
-  match do |s|
-    @s = s
-    JSON.load(JSON.dump(s)).eql? s
+  match do |subject|
+    to_json_and_back(subject).eql? subject
   end
 
-  failure_message_for_should do |actual|
-    "expected #{@s.inspect} to be eql after json dump-load\n\tgot     #{actual.inspect} instead"
+  failure_message_for_should do |subject|
+    <<HERE
+expected #{subject.inspect} to be eql after json dump-load
+got      #{to_json_and_back(subject).inspect} instead"
+HERE
+  end
+
+  failure_message_for_should_not do |subject|
+    <<HERE
+expected #{subject.inspect} not to be eql after json dump-load
+(got     #{to_json_and_back(subject).inspect} after dump/load)"
+HERE
   end
 
   description do
-    "be the same object after JSON.dump and JSON.load"
+    "be the eql after JSON.dump and JSON.load"
   end
 end
